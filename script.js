3cece9e9b929b8c6ab59dae068881608c5ea725b
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2023-04-21 08:55:55 +0300

    Add event to -/+ button

--- script.js
+++ script.js
@@ -38,6 +38,7 @@ const operators = document.querySelectorAll('.operator');
 const equals = document.querySelector('button[data-key="="]');
 const clear = document.querySelector('button[data-key="AC"]');
 const del = document.querySelector('button[data-key="del"]');
+const negative = document.querySelector('button[data-key="-/+"]');
 
 operators.forEach(operator => {
     operator.addEventListener('click',()=> {
@@ -66,4 +67,12 @@ clear.addEventListener('click', () => {
 
 del.addEventListener('click', () => {
     display.textContent = display.textContent.slice(0, display.textContent.length - 1);
+});
+
+negative.addEventListener('click', () => {
+    if (display.textContent.match(/^[0-9]*$/)) {
+        display.textContent = '-' + display.textContent;
+    } else if (display.textContent.match(/^-[0-9]*$/)) {
+        display.textContent = display.textContent.slice(1);
+    }
 });
