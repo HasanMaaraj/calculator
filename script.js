08167e1c10ce64c8a0888c408fd480b6af502aa9
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2023-04-21 07:05:08 +0300

    Create operate function

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 function add (a,b) {
-    return a+b
+    return (a+b);
 }
 
 function substract (a,b) {
-    return a-b
+    return (a-b);
 }
 
 function multiply (a,b) {
-    return a*b
+    return (a*b);
 }
 
 function divide (a,b) {
-    return a/b
+    return (a/b);
+}
+
+function operate (expression) {
+    if (expression.includes('+')) {
+        let numbers = expression.split('+');
+        return add(Number(numbers[0]), Number(numbers[1]));
+    } else if (expression.includes('-')) {
+        let numbers = expression.split('-');
+        return substract(numbers[0], numbers[1]);
+    } else if (expression.includes('*')) {
+        let numbers = expression.split('*');
+        return multiply(numbers[0], numbers[1]);
+    } else if (expression.includes('/')) {
+        let numbers = expression.split('/');
+        return divide(numbers[0], numbers[1]);
+    }
 }
